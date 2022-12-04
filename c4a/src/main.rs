bf6548fff269898c916ccae6a502d7480c7ea59b
Thomas Rinsma <thomasrinsma@gmail.com>
2022-12-04 11:20:42 +0100

    day 4

---
+++ c4a/src/main.rs
@@ -0,0 +1,25 @@
+use regex::Regex;
+
+fn main() {
+    let input = include_str!("in.txt");
+
+    let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
+
+    let result = input
+        .lines()
+        .filter(|line| {
+            let caps = re.captures(line).unwrap();
+
+            // parse as i32
+            let p = |x: &str| x.parse::<i32>().unwrap();
+
+            let res = (p(&caps[1]) >= p(&caps[3]) && p(&caps[2]) <= p(&caps[4]))
+                         || (p(&caps[1]) <= p(&caps[3]) && p(&caps[2]) >= p(&caps[4]));
+
+            return res;
+        })
+        .count();
+
+    
+    println!("result: {}", result);
+}
